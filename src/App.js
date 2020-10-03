f01b76b27c4e49a3d44aaa705cf114c17475cbfa
FranklinUmeObi <franklinumeobi@gmail.com>
2020-10-03 17:09:24 +0100

    Project Before MERN Is Implemented

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+
+import Header from './Components/Core/Header'
+import Footer from './Components//Core/Footer'
+import MainBody from './Components//Core/MainBody'
+
+function App() {
+  return (
+
+  <div className="App">
+    <Header/>
+    <MainBody/>
+    <Footer/>
+  </div>
+  
+  );
+}
+
+export default App;
