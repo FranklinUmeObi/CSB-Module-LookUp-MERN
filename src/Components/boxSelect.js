6038b67f6c5ad4e465724ca14737acb98439678c
FranklinUmeObi <franklinumeobi@gmail.com>
2020-10-04 13:29:35 +0100

    Refactored BoxSelect

--- src/Components/boxSelect.js
+++ src/Components/boxSelect.js
@@ -1,87 +1,86 @@
 import React from "react";
 
-import Button from '@material-ui/core/Button';
+import Button from "@material-ui/core/Button";
 
 import "./boxSelect.css";
-import data from '../Data/CourseData.json'
 
-import CardList from './cardList'
-import Sem1List from './sem1List'
-import Sem2List from './sem2List'
-
-
+import CardList from "./cardList";
+import Sem1List from "./sem1List";
+import Sem2List from "./sem2List";
 
 function BoxSelect(props) {
-
   //--------------------------------------------------
   //State
-  const [state, setState] = React.useState(
-    {
-      selected: 
-      [false, false, false, false, false, false, false, false, false, false,
-        false, false, false, false, false, false, false, false, false, false,
-        false, false, false, false, false, false, false, false, false ],
-      listVisibility:false
-    }
-  );
-
-
-//--------------------------------------------------
-//Functions
-
-  const onShow = event => {
-    setState({...state, listVisibility: true});
-  }
-
-
-
-//--------------------------------------------------
-//Semester 1 Checkboxes
-
-  if (props.semester === "1")
-  {
-    return (
-      <div className="boxSelect">
-
-        <Sem1List state={state} setState={setState}/>
-
-        <div className="buttonContainer">
-          <Button className="button" size="large" onClick={onShow} variant="contained" color="secondary">
-            Submit
-          </Button>
-        </div>
-
-        <CardList show={state.listVisibility} data={props.data} state={state}/>
-
-      </div>
-    );
-  }
-
-
-
-
+  const [state, setState] = React.useState({
+    selected: [
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+    ],
+    listVisibility: false,
+  });
 
   //--------------------------------------------------
-  //Semester 2 Checkboxes
+  //Functions
 
-  else {
-    return(
-      <div className="boxSelect">
-        <Sem2List state={state} setState={setState}/>
+  const onShow = (event) => {
+    setState({ ...state, listVisibility: true });
+  };
 
-        <div className="buttonContainer">
-          <Button className="button" size="large" onClick={onShow} variant="contained" color="secondary">
-            Submit
-          </Button>
-        </div>
+  //--------------------------------------------------
+  //Variables
+  let ModuleList;
+  if (props.semester === "1")
+    ModuleList = <Sem1List state={state} setState={setState} />;
+  else 
+    ModuleList = <Sem2List state={state} setState={setState} />;
 
-        <CardList show={state.listVisibility} course={data} state={state}/>
+  //--------------------------------------------------
+  // Checkboxes
 
+  return (
+    <div className="boxSelect">
+      {ModuleList}
+      <div className="buttonContainer">
+        <Button
+          className="button"
+          size="large"
+          onClick={onShow}
+          variant="contained"
+          color="secondary"
+        >
+          Submit
+        </Button>
       </div>
-    );
-  }
+
+      <CardList show={state.listVisibility} data={props.data} state={state} />
+    </div>
+  );
 }
-
-
-
 export default BoxSelect;
